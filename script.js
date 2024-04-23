1be7f4d3b6e145bd50e2b10f3cb2295f3309b598
Shreyashrazz <164854166+Shreyashrazz@users.noreply.github.com>
2024-04-23 21:37:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let x = document.querySelector('.nav-list');
+let micon = document.querySelector('.menu-icon');
+let cicon = document.querySelector('.cross');
+
+function openMenu(){
+   x.computedStyleMap.display ='block';
+}
+
